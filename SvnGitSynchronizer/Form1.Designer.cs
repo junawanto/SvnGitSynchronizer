9eb72e5525fef2ed4e63f70bdadc509aa6621123
jun <Jun@mail.com>
2019-11-14 14:00:06 +0700

    git committed by system

---
+++ SvnGitSynchronizer/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SvnGitSynchronizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTortoiseLocation = new System.Windows.Forms.TextBox();
+            this.txtGitAddress = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Git Address :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SVN Tortoise Location :";
+            // 
+            // txtTortoiseLocation
+            // 
+            this.txtTortoiseLocation.Location = new System.Drawing.Point(146, 12);
+            this.txtTortoiseLocation.Name = "txtTortoiseLocation";
+            this.txtTortoiseLocation.Size = new System.Drawing.Size(224, 20);
+            this.txtTortoiseLocation.TabIndex = 4;
+            // 
+            // txtGitAddress
+            // 
+            this.txtGitAddress.Location = new System.Drawing.Point(146, 41);
+            this.txtGitAddress.Name = "txtGitAddress";
+            this.txtGitAddress.Size = new System.Drawing.Size(224, 20);
+            this.txtGitAddress.TabIndex = 5;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(177, 86);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(95, 35);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "&Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 144);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtGitAddress);
+            this.Controls.Add(this.txtTortoiseLocation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTortoiseLocation;
+        private System.Windows.Forms.TextBox txtGitAddress;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ SyncRepo/Method.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using SharpSvn;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+
+namespace SyncRepo
+{
+    public class Method
+    {
+        public static string GetInfo(string location)
+        {
+            string doCommand = string.Empty;
+            string result = string.Empty;
+            string serverPath = string.Empty;
+            string localPath = string.Empty;
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe");
+
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.StandardInput.WriteLine(@"cd " + location);
+                process.StandardInput.WriteLine("svn info --xml > serverInfoTmp.xml");
+                process.StandardInput.WriteLine("svn log -r head:0 --xml > localInfoTmp.xml");
+                process.StandardInput.WriteLine("/C");
+                process.WaitForExit();
+
+                serverPath = location + "\\serverInfoTmp.xml";
+                localPath = location + "\\localInfoTmp.xml";
+
+                doCommand = CompareRevision(serverPath, localPath);
+                result = ProcessCommand(doCommand, location);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return result;
+        }
+
+        public static string CompareRevision(string serverPath, string localPath)
+        {
+            string command = string.Empty;
+            List<string> lsServerLogRevision = new List<string>();
+            List<string> lsCientLogRevision = new List<string>();
+
+            #region server side
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(serverPath);
+
+            XmlNodeList xmlServerNodeList = xmlDoc.SelectNodes("/info/entry/commit");
+            foreach (XmlNode xn in xmlServerNodeList)
+            {
+                string logRevision = xn.Attributes[0].Value;
+                lsServerLogRevision.Add(logRevision);
+            }
+            string serverRevision = lsServerLogRevision[0];
+            #endregion
+            #region client side
+            xmlDoc.Load(localPath);
+
+            XmlNodeList xmlClientNodeList = xmlDoc.SelectNodes("/log/logentry");
+            foreach (XmlNode xn in xmlClientNodeList)
+            {
+                string logRevision = xn.Attributes[0].Value;
+                lsCientLogRevision.Add(logRevision);
+            }
+            string clientRevision = lsCientLogRevision[0];
+            #endregion
+
+            if (Int32.Parse(serverRevision) > Int32.Parse(clientRevision))
+                command = "update";
+            else if (Int32.Parse(serverRevision) < Int32.Parse(clientRevision))
+                command = "commit";
+            else if (Int32.Parse(serverRevision) == Int32.Parse(clientRevision))
+                command = "do nothing";
+
+            return command;
+        }
+
+        public static string ProcessCommand(string command, string path)
+        {
+            string result = string.Empty;
+            if (command == "update")
+            {
+                SvnUpdateResult svnResult;
+                SvnUpdateArgs args = new SvnUpdateArgs();
+
+                using (SvnClient client = new SvnClient())
+                {
+                    client.Update(path, args, out svnResult);
+                }
+                result = "updated";
+            }
+            else if (command == "commit")
+            {
+                SvnCommitArgs args = new SvnCommitArgs();
+                args.LogMessage = "commited by system at " + DateTime.Now;
+
+                using (SvnClient client = new SvnClient())
+                {
+                    client.Commit(path, args);
+                }
+                result = "commited";
+            }
+            else if (command == "do nothing")
+            {
+                result = "nothing changed";
+            }
+
+            return result;
+        }
+
+        public static bool CompareFile(string file1, string file2)
+        {
+            try
+            {
+                byte[] byte1 = File.ReadAllBytes(file1);
+                byte[] byte2 = File.ReadAllBytes(file2);
+
+                if (file1.Length == file2.Length)
+                {
+                    for (int i = 0; i < file1.Length; i++)
+                    {
+                        if (file1[i] != file2[i])
+                        {
+                            return false;
+
+                        }
+                    }
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            
+        }
+    }
+}
